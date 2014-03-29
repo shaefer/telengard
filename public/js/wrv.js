11d47f0131f7c6ae921914fc95948f75880519d8
shaefer <shaefer@gmail.com>
2014-03-29 14:51:11 -0500

    updated room ids to use their own seed from the walls.

--- public/js/wrv.js
+++ public/js/wrv.js
@@ -22,11 +22,13 @@ function Room(x,y,z) {
     this.x = x;
     this.y = y;
     this.z = z;
+    Math.seedrandom(x.toString() + y.toString() + z.toString());
+    this.id = Math.random();
     this.getNorthWall = function () {
         var self = this;
         return new Wall(new Vertex(self.x, self.y, self.z), new Vertex(self.x + 1, self.y, self.z))
     }
 };
 Room.prototype.toString = function () {
-    return "(" + this.x + ", " + this.y + ", " + this.z + "), North Wall: " + this.getNorthWall().toString()
+    return "(" + this.x + ", " + this.y + ", " + this.z + "), Room Id: " + this.id + ", North Wall: " + this.getNorthWall().toString()
 };
