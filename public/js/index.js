7997764be617a78affabf02b6c7910e8d703e6ea
shaefer <shaefer@gmail.com>
2014-03-29 22:22:27 -0500

    added in grid and hooked in drawing of table based grid.

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,37 @@ $(document).ready(init);
 function init()
 {
     console.warn('init');
+
+    var width = 10;
+    var height = 10;
+    var maxWidthIndex = width - 1;
+    var maxHeightIndex = height - 1;
+    var table = $('<table>');
+    table.addClass('dungeon');
+    for (var y = 0; y <= maxWidthIndex;y++ )
+    {
+        var row = $('<tr>');
+        table.append(row);
+        for (var x = 0; x <= maxHeightIndex;x++ )
+        {
+            var cell = $('<td>');
+            row.append(cell);
+            cell.addClass("x" + x + "y" + y)
+            var room = new Room(x, y, 0);
+            console.warn(room.toString())
+            console.warn(room.getNorthWall().hasWall());
+            if (y == 0 || room.getNorthWall().hasWall())
+                cell.addClass("northWall")
+            if (x == maxWidthIndex || room.getEastWall().hasWall())
+                cell.addClass("eastWall")
+            if (y == maxHeightIndex || room.getSouthWall().hasWall())
+                cell.addClass("southWall")
+            if (x == 0 || room.getWestWall().hasWall())
+                cell.addClass("westWall")
+        }        
+    }
+    table.appendTo($('.level'))
 }
 
-for (var x = 0; x <= 10;x++ )
-{
-    for (var y = 0; y <= 10;y++ )
-    {
-        console.warn(new Room(x, y, 0).toString()) 
-    }        
-}
+
         
--- public/js/wrv.js
+++ public/js/wrv.js
@@ -1,3 +1,6 @@
+//http://pcg.wikidot.com/pcg-algorithm:dungeon-generation
+//http://pcg.wdfiles.com/local--files/pcg-algorithm%3Amaze/growingtree.py
+
 function Vertex(x,y,z) {
     this.x = x;
     this.y = y;
@@ -12,10 +15,15 @@ function Wall(vertex1, vertex2) {
     this.vertex1 = vertex1;
     this.vertex2 = vertex2;
     Math.seedrandom(this.vertex1.toString() + this.vertex2.toString());
-    this.id = Math.random();
+    this.id = Math.random().toString().substring(2);
 };
 Wall.prototype.toString = function () {
     return "(" + this.vertex1.toString() + ", " + this.vertex2.toString() + "; WallId: " + this.id + ")";
+};
+Wall.prototype.hasWall = function () {
+    var firstChar = Number(this.id.toString().substring(0, 1))
+    console.debug(firstChar)
+    return firstChar > 6
 };
 
 function Room(x,y,z) {
@@ -24,9 +32,37 @@ function Room(x,y,z) {
     this.z = z;
     Math.seedrandom(x.toString() + y.toString() + z.toString());
     this.id = Math.random();
+    this.nwVertex = function () {
+        var self = this;
+        return new Vertex(self.x, self.y, self.z)
+    };
+    this.neVertex = function () {
+        var self = this;
+        return new Vertex(self.x + 1, self.y, self.z)
+    };
+    this.seVertex = function () {
+        var self = this;
+        return new Vertex(self.x + 1, self.y + 1, self.z)
+    };
+    this.swVertex = function () {
+        var self = this;
+        return new Vertex(self.x, self.y + 1, self.z)
+    };
     this.getNorthWall = function () {
         var self = this;
-        return new Wall(new Vertex(self.x, self.y, self.z), new Vertex(self.x + 1, self.y, self.z))
+        return new Wall(self.nwVertex(), self.neVertex())
+    }
+    this.getEastWall = function () {
+        var self = this;
+        return new Wall(self.neVertex(), self.seVertex())
+    }
+    this.getSouthWall = function () {
+        var self = this;
+        return new Wall(self.swVertex(), self.seVertex())
+    }
+    this.getWestWall = function () {
+        var self = this;
+        return new Wall(self.nwVertex(), self.swVertex())
     }
 };
 Room.prototype.toString = function () {
