a9d13a2e49d4c000ee2894134227377089abd373
Daniel Shaefer <daniel@dwolla.com>
2014-04-04 15:06:34 -0500

    extracted map into draw method that takes a center point and a radius for drawing.

--- public/js/index.js
+++ public/js/index.js
@@ -3,29 +3,47 @@ function init()
 {
     console.warn('init');
 
+    draw(2,2,0, 2)
+}
+
+function draw(x,y,z,radius)
+{
+    if (!radius)
+    {
+        console.warn('no radius was specified')
+        radius = 5;
+    }
+
     var width = 10;
     var height = 10;
     var maxWidthIndex = width - 1;
     var maxHeightIndex = height - 1;
+
+    var cx = x - radius > 0 ? x - radius : 0;
+    var cy = y - radius > 0 ? x - radius : 0;
+    var mx = x + radius < maxHeightIndex ? x + radius : maxHeightIndex;
+    var my = y + radius < maxWidthIndex ? y + radius : maxWidthIndex;
+    var cz = z;
+
     var table = $('<table>');
     table.addClass('dungeon');
-    for (var y = 0; y <= maxWidthIndex;y++ )
+    for (var y = cy; y <= my;y++ )
     {
         var row = $('<tr>');
         table.append(row);
-        for (var x = 0; x <= maxHeightIndex;x++ )
+        for (var x = cx; x <= mx;x++ )
         {
             var cell = $('<td>');
             row.append(cell);
             cell.addClass("x" + x + "y" + y)
-            var room = new Room(x, y, 0);
+            var room = new Room(x, y, cz);
             console.warn(room.toString())
             console.warn(room.getNorthWall().hasWall());
             if (y == 0 || room.getNorthWall().hasWall())
                 cell.addClass("northWall")
-            if (x == maxWidthIndex || room.getEastWall().hasWall())
+            if (x == my || room.getEastWall().hasWall())
                 cell.addClass("eastWall")
-            if (y == maxHeightIndex || room.getSouthWall().hasWall())
+            if (y == mx || room.getSouthWall().hasWall())
                 cell.addClass("southWall")
             if (x == 0 || room.getWestWall().hasWall())
                 cell.addClass("westWall")
