90312c9c1e96a7db30dcfd3e3193155fbe3eb468
Daniel Shaefer <daniel@dwolla.com>
2014-04-10 16:32:54 -0500

    split out utility functions into their own file. fixed some meta tag values

--- public/js/index.js
+++ public/js/index.js
@@ -7,26 +7,3 @@ function init() {
     var end = new Date();
     console.warn('Time to draw: ' + (end.getTime() - start.getTime()) + "ms");
 }
-
-function GetIdChar(rand, index) {
-    console.warn("Index for GetIdChar: " + index);
-    return Number(rand.toString().substring(index, index + 1));
-}
-
-Number.prototype.padLeft = function(base,chr){
-    var  len = (String(base || 10).length - String(this).length)+1;
-    return len > 0? new Array(len).join(chr || '0')+this : this;
-}
-
-function toTimestamp(d) {
-    var dformat = [
-    (d.getMonth()+1).padLeft(),
-    d.getDate().padLeft(),
-    d.getFullYear()].join('/')+
-    ' ' +
-    [d.getHours().padLeft(),
-    d.getMinutes().padLeft(),
-    d.getSeconds().padLeft()].join(':');
-    return dformat;
-}
-
