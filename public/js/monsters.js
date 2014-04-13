55c66039187dc32d0b3face0c23532b2c966ae3c
shaefer <shaefer@gmail.com>
2014-04-12 21:33:03 -0500

    added hp function builder to help with simplicity of new creature entry

--- public/js/monsters.js
+++ public/js/monsters.js
@@ -38,16 +38,20 @@ var BuildMonster = function(varName, name, src, width, height, hpFunc) {
 	})
 };
 
+var HpFuncBuilder = function(num, sides, mod) {
+	return function() {return DiceUtils.roll(num, sides, mod).total;};
+};
+
 var Monsters = {};
-BuildMonster("DireRat", "Dire Rat", "/images/rat_dire__ryan_sumo.png", 212/4, 241/4, function() {return DiceUtils.roll(1, 10, 30).total;});
-BuildMonster("OrcLeader", "Orc Leader", "/images/orc_king__eric_quigley.png", 240/1.8, 300/1.8, function() {return DiceUtils.roll(2, 10, 30).total;});
-BuildMonster("FeralTroll", "Feral Troll", "/images/troll_feral__forrest_imel.png", 300/1.5, 256/1.5, function() {return DiceUtils.roll(2, 10, 60).total;});
-BuildMonster("Behir", "Behir", "/images/behir__bruno_balixa_lq.png", 350/1.5, 361/1.5, function() {return DiceUtils.roll(2, 10, 80).total;});
-BuildMonster("RedDragon", "Red Dragon", "/images/dragon_red__malcolm_mcclinton_nobg_lq.png", 500, 353, function() {return DiceUtils.roll(3, 10, 100).total;});
-BuildMonster("WarTroll", "War Troll", "/images/troll_warrior__bruno_balixa.png", 236/1.5, 300/1.5, function() {return DiceUtils.roll(2, 10, 70).total;});
-BuildMonster("Basilisk", "Basilisk", "/images/basilisk__storn_cook.png", 600/3, 346/3, function() {return DiceUtils.roll(2, 10, 50).total;});
-BuildMonster("DireBat", "Dire Bat", "/images/bat_dire__nicole_cardiff.png", 300/3, 232/3, function() {return DiceUtils.roll(2, 10, 20).total;});
-BuildMonster("KoboldMerc", "Kobold Mercenary", "/images/kobold_wsword__bruno_balixa.png", 139, 150, function() {return DiceUtils.roll(2, 10, 20).total;});
-BuildMonster("Skeleton", "Iron Skeleton", "/images/skeleton_2__bruno_balixa.png", 277/1.8, 300/1.8, function() {return DiceUtils.roll(3, 10, 10).total;});
-BuildMonster("IronGolem", "Iron Golem", "/images/golem_iron__bruno_balixa.png", 244, 300, function() {return DiceUtils.roll(3, 10, 100).total;});
-BuildMonster("DireCrocodile", "Dire Crocodile", "/images/crocodile_dire__malcolm_mcclinton.png", 300/1.5, 218/1.5, function() {return DiceUtils.roll(3, 10, 40).total;});
+BuildMonster("DireRat", "Dire Rat", "/images/rat_dire__ryan_sumo.png", 212/4, 241/4, HpFuncBuilder(1, 10, 30));
+BuildMonster("OrcLeader", "Orc Leader", "/images/orc_king__eric_quigley.png", 240/1.8, 300/1.8, HpFuncBuilder(2, 10, 30));
+BuildMonster("FeralTroll", "Feral Troll", "/images/troll_feral__forrest_imel.png", 300/1.5, 256/1.5, HpFuncBuilder(2, 10, 60));
+BuildMonster("Behir", "Behir", "/images/behir__bruno_balixa_lq.png", 350/1.5, 361/1.5, HpFuncBuilder(2, 10, 80));
+BuildMonster("RedDragon", "Red Dragon", "/images/dragon_red__malcolm_mcclinton_nobg_lq.png", 500, 353, HpFuncBuilder(3, 10, 100));
+BuildMonster("WarTroll", "War Troll", "/images/troll_warrior__bruno_balixa.png", 236/1.5, 300/1.5, HpFuncBuilder(2, 10, 70));
+BuildMonster("Basilisk", "Basilisk", "/images/basilisk__storn_cook.png", 600/3, 346/3, HpFuncBuilder(2, 10, 50));
+BuildMonster("DireBat", "Dire Bat", "/images/bat_dire__nicole_cardiff.png", 300/3, 232/3, HpFuncBuilder(2, 10, 20));
+BuildMonster("KoboldMerc", "Kobold Mercenary", "/images/kobold_wsword__bruno_balixa.png", 139, 150, HpFuncBuilder(2, 10, 20));
+BuildMonster("Skeleton", "Iron Skeleton", "/images/skeleton_2__bruno_balixa.png", 277/1.8, 300/1.8, HpFuncBuilder(3, 10, 10));
+BuildMonster("IronGolem", "Iron Golem", "/images/golem_iron__bruno_balixa.png", 244, 300, HpFuncBuilder(3, 10, 100));
+BuildMonster("DireCrocodile", "Dire Crocodile", "/images/crocodile_dire__malcolm_mcclinton.png", 300/1.5, 218/1.5, HpFuncBuilder(3, 10, 40));
